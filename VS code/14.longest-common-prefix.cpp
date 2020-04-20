5e94957b3cc2116a850f8d88d595dff87eefec17
liying8040 <liying8040@gmail.com>
2020-04-19 17:57:58 -0700

    leetcode vsc plugin

---
+++ VS code/14.longest-common-prefix.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=14 lang=cpp
+ *
+ * [14] Longest Common Prefix
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        
+    }
+};
+// @lc code=end
+
